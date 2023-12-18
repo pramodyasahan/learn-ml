431e7f73dc5aa349f0088f05a02fe614009736c5
pramodyasahan <sahandil1013@gmail.com>
2023-12-18 14:52:56 +0530

    feat: split the data set in to test and train

--- classification/logistic-regression/main.py
+++ classification/logistic-regression/main.py
@@ -11,4 +11,5 @@ dataset = pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 

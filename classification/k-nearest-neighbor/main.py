4583456a392e4fe5bf3bf792cb2bde9bb86b0393
pramodyasahan <sahandil1013@gmail.com>
2023-12-18 15:43:22 +0530

    feat: import libraries

---
+++ classification/k-nearest-neighbor/main.py
@@ -0,0 +1,9 @@
+# Importing necessary libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score

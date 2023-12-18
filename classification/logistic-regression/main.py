88dd095cb1a36dd586d9cc4bf5959c424f09f76d
pramodyasahan <sahandil1013@gmail.com>
2023-12-18 14:54:32 +0530

    feat: testing accuracy

--- classification/logistic-regression/main.py
+++ classification/logistic-regression/main.py
@@ -19,3 +19,12 @@ X_test = sc.transform(X_test)
 
 classifier = LogisticRegression(random_state=0)
 classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+accuracy = accuracy_score(y_test, y_pred)
+print(accuracy)

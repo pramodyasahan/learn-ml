aca9cddfaf3b0a2f62e1b1d8b5a170868a6b0901
pramodyasahan <sahandil1013@gmail.com>
2023-12-18 19:08:56 +0530

    feat: add comments

---
+++ classification/kernel-svm/main.py
@@ -0,0 +1,72 @@
+# Importing necessary libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+# Loading the dataset from a CSV file
+dataset = pd.read_csv('Social_Network_Ads.csv')
+
+# Extracting features (X) and target variable (y) from the dataset
+X = dataset.iloc[:, :-1].values  # Selecting all rows and all but the last column as features
+y = dataset.iloc[:, -1].values  # Selecting all rows and only the last column as the target variable
+
+# Splitting the dataset into Training and Test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)  # 25% data for testing
+
+# Feature Scaling
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Creating a SVC model
+classifier = SVC(kernel='rbf', random_state=0)
+classifier.fit(X_train, y_train)  # Training the model on the training set
+
+# Predicting the target variable for the test set
+y_pred = classifier.predict(X_test)
+
+# Printing the predicted and actual values side by side
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+# Creating a confusion matrix and calculating accuracy
+cm = confusion_matrix(y_test, y_pred)
+print(cm)  # Printing the confusion matrix
+accuracy = accuracy_score(y_test, y_pred)
+print(accuracy)  # Printing the accuracy of the model
+
+# Visualizing the results on the Training set
+X_set, y_set = sc.inverse_transform(X_train), y_train
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 10, stop=X_set[:, 0].max() + 10, step=0.5),
+                     np.arange(start=X_set[:, 1].min() - 1000, stop=X_set[:, 1].max() + 1000, step=0.5))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha=0.75, cmap=ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c=ListedColormap(('red', 'green'))(i), label=j)
+plt.title('Logistic Regression (Training Set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualizing the results on the Testing set
+X_set, y_set = sc.inverse_transform(X_test), y_test
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 10, stop=X_set[:, 0].max() + 10, step=0.5),
+                     np.arange(start=X_set[:, 1].min() - 1000, stop=X_set[:, 1].max() + 1000, step=0.5))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha=0.75, cmap=ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c=ListedColormap(('red', 'green'))(i), label=j)
+plt.title('Logistic Regression (Testing Set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()

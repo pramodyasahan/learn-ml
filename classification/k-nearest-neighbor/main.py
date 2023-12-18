9861b2e800f6da2f846b646b6076c8417a1228c2
pramodyasahan <sahandil1013@gmail.com>
2023-12-18 15:43:44 +0530

    feat: import the dataset

--- classification/k-nearest-neighbor/main.py
+++ classification/k-nearest-neighbor/main.py
@@ -7,3 +7,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix, accuracy_score
+
+# Loading the dataset from a CSV file
+dataset = pd.read_csv('Social_Network_Ads.csv')
+
+# Extracting features (X) and target variable (y) from the dataset
+X = dataset.iloc[:, :-1].values  # Selecting all rows and all but the last column as features
+y = dataset.iloc[:, -1].values  # Selecting all rows and only the last column as the target variable
+

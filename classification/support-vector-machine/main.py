c0b6c2ef60a746375f043f546b8ed9f0c11b8686
pramodyasahan <sahandil1013@gmail.com>
2023-12-18 15:55:23 +0530

    feat: import libraries

---
+++ classification/support-vector-machine/main.py
@@ -0,0 +1,10 @@
+# Importing necessary libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix, accuracy_score
+

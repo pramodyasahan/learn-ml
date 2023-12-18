68fb6c2c108c99ff1b414e5b814a0787cabb7cfe
pramodyasahan <sahandil1013@gmail.com>
2023-12-18 14:52:09 +0530

    feat: import libraries

---
+++ classification/logistic-regression/main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+

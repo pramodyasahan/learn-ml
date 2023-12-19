e9a6e8ba30a5bda2ce1642f449fb04b43f15c1f1
pramodyasahan <sahandil1013@gmail.com>
2023-12-19 15:11:16 +0530

    feat: add comments

--- clustering/k-means-clustering/main.py
+++ clustering/k-means-clustering/main.py
@@ -3,24 +3,29 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.cluster import KMeans
 
+# Loading the dataset from a CSV file
 dataset = pd.read_csv('Mall_Customers.csv')
-X = dataset.iloc[:, [3, 4]].values
+X = dataset.iloc[:, [3, 4]].values  # Selecting only the relevant features (Annual Income and Spending Score)
 
+# Calculating WCSS (Within-Cluster Sum of Square) for different numbers of clusters
 wcss = []
 for i in range(1, 11):
     cluster = KMeans(n_clusters=i, init='k-means++', random_state=42)
     cluster.fit(X)
-    wcss.append(cluster.inertia_)
+    wcss.append(cluster.inertia_)  # Inertia: Sum of distances of samples to their closest cluster center
 
+# Plotting the Elbow Method graph
 plt.plot(range(1, 11), wcss)
 plt.title('The Elbow Method')
 plt.xlabel('Number of Clusters')
 plt.ylabel('WCSS')
-plt.show()
+plt.show()  # This plot helps to determine the optimal number of clusters
 
+# Applying KMeans to the dataset with the optimal number of clusters
 cluster = KMeans(n_clusters=5, init='k-means++', random_state=42)
 y_kmeans = cluster.fit_predict(X)
 
+# Visualizing the clusters
 plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], color='indigo', s=100, label='Cluster 1')
 plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], color='blue', s=100, label='Cluster 2')
 plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], color='orange', s=100, label='Cluster 3')

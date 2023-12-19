1468ff38f836465950c09830c293980f6982f103
pramodyasahan <sahandil1013@gmail.com>
2023-12-19 15:09:05 +0530

    feat: calculate WCSS

--- clustering/k-means-clustering/main.py
+++ clustering/k-means-clustering/main.py
@@ -6,4 +6,10 @@ from sklearn.cluster import KMeans
 dataset = pd.read_csv('Mall_Customers.csv')
 X = dataset.iloc[:, [3, 4]].values
 
+wcss = []
+for i in range(1, 11):
+    cluster = KMeans(n_clusters=i, init='k-means++', random_state=42)
+    cluster.fit(X)
+    wcss.append(cluster.inertia_)
+
 

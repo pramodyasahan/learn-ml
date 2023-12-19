5d0cad14fc049361aafc5c20b090ee9db365393d
pramodyasahan <sahandil1013@gmail.com>
2023-12-19 18:04:37 +0530

    feat: make transaction list

--- association-rule-learning/apriori/main.py
+++ association-rule-learning/apriori/main.py
@@ -3,3 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 dataset = pd.read_csv('Market_Basket_Optimisation.csv', header=None)
+transactions = []
+for raw in range(0, 7501):
+    transactions.append([str(dataset.values[raw, j]) for j in range(0, 20)])

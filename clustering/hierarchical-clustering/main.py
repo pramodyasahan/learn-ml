6d3472141e02befb03f5570e0beefe16be06da7e
pramodyasahan <sahandil1013@gmail.com>
2023-12-19 15:54:03 +0530

    feat: plot the dendrogram

--- clustering/hierarchical-clustering/main.py
+++ clustering/hierarchical-clustering/main.py
@@ -5,3 +5,9 @@ import scipy.cluster.hierarchy as sch
 
 dataset = pd.read_csv('Mall_Customers.csv')
 X = dataset.iloc[:, [3, 4]].values
+
+dendrogram = sch.dendrogram(sch.linkage(X, method='ward'))
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean Distance')
+plt.show()

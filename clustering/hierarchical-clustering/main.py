fd8e97a2b374e7077e5f51bd5dce66c08678b97c
pramodyasahan <sahandil1013@gmail.com>
2023-12-19 15:57:06 +0530

    feat: build the model

--- clustering/hierarchical-clustering/main.py
+++ clustering/hierarchical-clustering/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.cluster import AgglomerativeClustering
 import scipy.cluster.hierarchy as sch
 
 dataset = pd.read_csv('Mall_Customers.csv')
@@ -11,3 +12,6 @@ plt.title('Dendrogram')
 plt.xlabel('Customers')
 plt.ylabel('Euclidean Distance')
 plt.show()
+
+hc = AgglomerativeClustering(n_clusters=5, affinity='euclidean', linkage='ward')
+y_hc = hc.fit_predict(X)

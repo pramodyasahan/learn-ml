e1e68dcb2582666e75b9f047eacc3f0ff4490335
pramodyasahan <sahandil1013@gmail.com>
2023-12-20 17:59:26 +0530

    feat: import the dataset

--- natural-language-processing/main.py
+++ natural-language-processing/main.py
@@ -4,3 +4,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score, confusion_matrix
 
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter='\t')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values

04e516b1495e4f4c2ff8936150876ade31242b43
pramodyasahan <sahandil1013@gmail.com>
2023-12-20 17:56:59 +0530

    feat: import the libraries

---
+++ natural-language-processing/main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score, confusion_matrix
+

9f8ffcff20f9052279ce562277057af70f7f453e
pramodyasahan <sahandil1013@gmail.com>
2023-12-20 18:18:49 +0530

    feat: clean the text

--- natural-language-processing/main.py
+++ natural-language-processing/main.py
@@ -1,9 +1,22 @@
+import re
+import nltk
 import pandas as pd
 import matplotlib.pyplot as plt
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score, confusion_matrix
 
-dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter='\t')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter='\t', quoting=3)
+
+nltk.download('stopwords')
+corpus = []
+for i in range(0, len(dataset)):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
